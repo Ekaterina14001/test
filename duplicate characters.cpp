ea1f7ff25e9b73be30767fbc30fc16d4e19b8209
Ekaterina14001 <39101719+Ekaterina14001@users.noreply.github.com>
2023-05-02 22:11:54 +0300

    Add files via upload

---
+++ duplicate characters.cpp
@@ -0,0 +1,32 @@
+﻿
+
+#include <iostream>
+
+void RemoveDups(char* str)
+{
+    
+    int i = 0;
+    int N = sizeof(str)+1;
+    while (i !=N+1)
+    {
+        if (str[i] == str[i + 1])
+        {
+            for (int j = i; j != N; j++)
+                str[j] = str[j + 1];
+            str[N+1] = ' ';
+            N--;
+        }
+        else i++;
+    }
+
+}
+
+int main()
+{
+    char data[] = "AAA BBB AAA";
+    RemoveDups(data);
+    printf("%s\n", data);
+}
+
+// Соколенко Екатерина Романовна
+// 26.04.2023 40 мин
---
+++ setbit.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void printBit(int num)
+{
+	std::vector <bool> Bit;
+	while (num > 1)
+	{
+		Bit.push_back(num % 2);
+		num = num / 2;
+	}
+	Bit.push_back(num);
+	std::reverse(Bit.begin(), Bit.end());
+
+	for (auto it = Bit.begin(); it != Bit.end(); it++)
+		std::cout << *it;
+	
+}
+
+int main()
+{
+    int num=123;
+	printBit(num);
+
+}
+
+// Соколенко Екатерина Романовна
+// 26.04.2023 30 мин
